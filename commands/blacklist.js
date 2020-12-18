832b0055bc4efc94d351e192ef3e20f74fe91b2a
Togira <stuhlvonachmed2@outlook.com>
2020-12-18 23:21:21 +0100

    added blacklist command

---
+++ commands/blacklist.js
@@ -0,0 +1,40 @@
+const database = require('../databases')
+const Discord = require('discord.js')
+
+module.exports = {
+    name: 'bl',
+    description: 'blacklist a word which will get auto-deleted when used.',
+    args: true,
+    execute(message, args) {
+        const Bl = async (message, args) => {
+            if (args[0] === 'add') {
+                const exists = await database.BannedWords.findOne({ where: { word: args[1] } })
+                if (exists) {
+                    return message.channel.send('This word is already listed!')
+                } try {
+                    const addTheWord = await database.BannedWords.create({
+                        word: args[1]
+                    })
+                    message.channel.send(`Successfully added the new word: ${args[1]}`)
+                } catch {
+                    return message.channel.send('Something went wrong :c')
+                }
+            } else if (args[0] === 'delete') {
+                const exists = await database.BannedWords.findOne({ where: { word: args[1] } })
+                if (!exists) {
+                    return message.channel.send('You can\'t delete a non-existing word')
+                } try {
+                    await database.BannedWords.destroy({ where: { word: args[1] } })
+                    message.channel.send(`Successfully deleted the word: ${args[1]}`)
+                } catch {
+                    return message.channel.send('Something went wrong :c')
+                }
+            } else if (!args.length) {
+                const allWords = await database.BannedWords.findAll()
+                const bannedWords = new Discord.MessageEmbed()
+                    .setDescription(allWords.join(', '))
+            }
+        }
+        Bl(message, args)
+    }
+}
