c6280de7bfe912bcef7f5184111cb3c66ba9fb0d
Togira123 <stuhlvonachmed2@outlook.com>
2021-04-13 00:23:25 +0200

    added some text so it's less random

--- commands/lockdown.js
+++ commands/lockdown.js
@@ -12,7 +12,17 @@ module.exports = {
          * @param {Array<String>} args 
          */
         const lockdown = async(message, args) => {
-            await message.guild.roles.everyone.setPermissions([]);
+            try {
+                await message.guild.roles.everyone.setPermissions(['VIEW_CHANNEL']);
+                message.guild.channels.cache.forEach(async c => {
+                    if (c.type === 'text') {
+                        await c.send('server lockdown! You will be notified when it\'s unlocked again!')
+                    }
+                })
+                await message.channel.send('Server locked!!')
+            } catch {
+                message.channel.send('An error happened :(');
+            }
         }
         lockdown(message, args);
     }
