a9e682a84a67a0bca9b79a3486856002bad95a2f
Togira123 <stuhlvonachmed2@outlook.com>
2021-04-13 00:30:01 +0200

    added release command

---
+++ commands/release.js
@@ -0,0 +1,45 @@
+const Discord = require('discord.js')
+
+module.exports = {
+    name: 'lockdown',
+    description: 'locks the whole server',
+    modOnly: true,
+
+    execute(message, args) {
+        /**
+         * 
+         * @param {Discord.Message} message 
+         * @param {Array<String>} args 
+         */
+        const release = async(message, args) => {
+            try {
+                await message.guild.roles.everyone.setPermissions(
+                    [
+                        'VIEW_CHANNEL',
+                        'CREATE_INSTANT_INVITE',
+                        'CHANGE_NICKNAME',
+                        'SEND_MESSAGES',
+                        'EMBED_LINKS',
+                        'ATTACH_FILES',
+                        'ADD_REACTIONS',
+                        'USE_EXTERNAL_EMOJIS',
+                        'READ_MESSAGE_HISTORY',
+                        'CONNECT',
+                        'SPEAK',
+                        'STREAM',
+                        'USE_VAD',
+                    ]
+                );
+                message.guild.channels.cache.forEach(async c => {
+                    if (c.type === 'text') {
+                        await c.send('server is open again!')
+                    }
+                })
+                await message.channel.send('Server released!!')
+            } catch {
+                message.channel.send('An error happened :(');
+            }
+        }
+        release(message, args);
+    }
+}
